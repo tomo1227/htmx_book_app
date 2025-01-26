8291a78b54a08bcfd6843c08e444c6502809a6cf
tomo1227 <tomo.soccer1227@gmail.com>
2025-01-26 15:01:05 +0000

    chatアプリの追加

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ from starlette.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
